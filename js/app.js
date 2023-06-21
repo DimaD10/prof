576e56ceae052a34d5296011db4fa7502f94e92f
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-06-21 18:17:35 +0300

    stage1 for programmer finished

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 
-
 window.addEventListener('load', () => {
     const devices = document.querySelectorAll('.device-manage');
     let devicesCount = devices.length;
+    const popup = document.querySelector('.popup');
+    const popupFinished = document.querySelector('.popup-finished');
 
     document.querySelectorAll('.devices-counter').forEach(el => {
         el.textContent = devicesCount;
     })
     checkProtect(devicesCount);
 
+    // for cards
+    sortCards();
+    let accountCards = document.querySelectorAll('.accounts-card_active');
+    document.getElementById('accounts-non-checked').textContent = accountCards.length;
+
+
     document.addEventListener('click', e => {
         if (e.target.dataset.step === 'finish') {
             finishStep();
         }
+        if (e.target.dataset.step === 'incorrect') {
+            popup.classList.add('_opened');
+            window.setTimeout(() => {
+                popup.classList.add('clickable');
+            }, 5);
+        }
+
     
         if (e.target.classList.contains('safety-protocol__button')) {
             e.target.classList.remove('button_active');
@@ -51,8 +65,72 @@ window.addEventListener('load', () => {
                 el.textContent = '[ 0 ]';
             })
         }
+
+        if (e.target.classList.contains('accounts-answears__button')) {
+            let cards = document.querySelectorAll('.accounts-card_active');
+            let answears = [...document.querySelectorAll('.accounts-answears__button')]
+            let choice = answears.indexOf(e.target);
+            let correctAnswear = document.querySelector('.accounts-card_current').dataset.correct;
+
+            if (choice === parseInt(correctAnswear)) {
+                document.querySelector('.accounts-card_current').classList.remove('accounts-card_active');
+                document.querySelector('.accounts-card_current').classList.remove('accounts-card_current');
+
+                cards = document.querySelectorAll('.accounts-card_active');
+
+                cards.forEach(el => {
+                    el.classList.remove('accounts-card_next')
+                })
+                if (cards.length - 1 >= 0) {
+                    cards[parseInt(cards.length - 1)].classList.add('accounts-card_current');
+                }
+                if (cards.length - 2 >= 0) {
+                    cards[parseInt(cards.length - 2)].classList.add('accounts-card_next');
+                }
+
+                sortCards();
+
+                if (cards.length === 0) {
+                    finishStep();
+                    document.querySelector('.accounts-answears').style.display = 'none'
+                    document.querySelector('.accounts-cards').innerHTML = `<p class="workflow__paragraph">Все данные актуализированы.</p><p class="workflow__paragraph">Вы проверили все учётные записи сотрудников!</p>`
+                }
+
+                document.getElementById('accounts-non-checked').textContent = cards.length;
+            } else {
+                callPopup(popup);
+            }
+        }
+
+        let activeSteps = document.querySelectorAll('.todo-steps__step_active');
+        let finishedSteps = document.querySelectorAll('.todo-steps__step_finished');
+
+        if (activeSteps.length === finishedSteps.length) {
+            popupFinished.classList.add('_opened');
+        }
     })
+    // for popup
+    document.addEventListener('click', e => {
+        if (!e.target.closest('.popup') && !e.target.closest('.button_low-priority') && popup.classList.contains('clickable') && popup.classList.contains('_opened')) {
+            popup.classList.remove('_opened');
+            popup.classList.remove('clickable');
+        }
+        if (e.target.closest('.popup__close-btn')) {
+            popup.classList.remove('_opened');
+            popup.classList.remove('clickable');
+        }
+    })
+    // for popup
+    document.addEventListener('keydown', function(event) {
+        if (popup.classList.contains('_opened') || popupFinished.classList.contains('_opened')) {
+            if (event.key == 'Escape') {
+                popup.classList.remove('_opened');
+                popup.classList.remove('clickable');
+            }
+        }
+    });
 })
+
 
 function finishStep() {
     let currentStep = document.querySelector('.todo-steps__step_current');
@@ -82,5 +160,26 @@ function checkProtect(devicesCount) {
         document.querySelector(".safety-manage").classList.add('safety-report__report_safe');
 
         finishStep()
+    }
+}
+
+function callPopup(popup) {
+    popup.classList.remove('clickable');
+    popup.classList.add('_opened');
+    window.setTimeout(() => {
+        popup.classList.add('clickable');
+    }, 5);
+}
+
+function sortCards() {
+    let accountCards = document.querySelectorAll('.accounts-card_active');
+    for (let i = 0; i < accountCards.length; i++) {
+        const el = accountCards[i];
+        let koef = 16 / (accountCards.length - 2);
+        el.style.transform = `translate(${(i) * koef}px, ${(i) * koef}px)`;
+
+        if (i + 1 === accountCards.length) {
+            document.querySelector('.accounts-cards').style.transform = `translateX(-${koef / 2}px)`
+        }
     }
 }
